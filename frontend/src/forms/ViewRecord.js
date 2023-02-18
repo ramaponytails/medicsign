b71a60d6cca9ddc7d9f034f35f2b38de5c111aab
MetalPower26 <MetalPower.github@gmail.com>
2023-02-18 23:50:01 +0700

    Clean View Record

--- frontend/src/forms/ViewRecord.js
+++ frontend/src/forms/ViewRecord.js
@@ -1,75 +1,77 @@
 import React, { Component } from "react";
 
 import {
-    Badge,
-    Button,
-    Card,
-    Navbar,
-    Nav,
-    Table,
-    Container,
-    Row,
-    Col,
-  } from "react-bootstrap";
+  Badge,
+  Button,
+  Card,
+  Navbar,
+  Nav,
+  Table,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
 
-class RecordView extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            doctor_name: "TEST DOCTOR NAME",
-            patient_name: "TEST PATIENT NAME",
-            medical_condition: "TEST MEDICAL CONDITION",
-            disease: "TEST DISEASE",
-            hospital_entry: "TEST ENTRY",
-            hospital_release: "TEST RELEASE",
-            record_number: "2834084",
-        }
-    }
-    render(){
-        return (
-            <Container fluid>
-                <Row>
-                    <Col>
-                        <Card className="card-plain table-plain-bg">
-                            <Card.Header>
-                                <Card.Title as="h4">Record {this.state.record_number}</Card.Title>
-                            </Card.Header>
-                            <Card.Body className="table-full-width table-responsive px-0">
-                                <Table className="table-hover">
-                                    <tbody>
-                                        <tr>
-                                            <td width="15%">Doctor Name</td>
-                                            <td>{this.state.doctor_name}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Patient Name</td>
-                                            <td>{this.state.patient_name}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Medical Condition</td>
-                                            <td>{this.state.medical_condition}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Disease</td>
-                                            <td>{this.state.disease}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Hospital Entry Date</td>
-                                            <td>{this.state.hospital_entry}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Hospital Release Date</td>
-                                            <td>{this.state.hospital_release}</td>
-                                        </tr>
-                                    </tbody>
-                                </Table>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
+class RecordView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      doctor_name: "TEST DOCTOR NAME",
+      patient_name: "TEST PATIENT NAME",
+      medical_condition: "TEST MEDICAL CONDITION",
+      disease: "TEST DISEASE",
+      hospital_entry: "TEST ENTRY",
+      hospital_release: "TEST RELEASE",
+      record_number: "2834084",
+    };
+  }
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col>
+            <Card className="card-plain table-plain-bg">
+              <Card.Header>
+                <Card.Title as="h4">
+                  Record {this.state.record_number}
+                </Card.Title>
+              </Card.Header>
+              <Card.Body className="table-full-width table-responsive px-0">
+                <Table className="table-hover">
+                  <tbody>
+                    <tr>
+                      <td width="15%">Doctor Name</td>
+                      <td>{this.state.doctor_name}</td>
+                    </tr>
+                    <tr>
+                      <td>Patient Name</td>
+                      <td>{this.state.patient_name}</td>
+                    </tr>
+                    <tr>
+                      <td>Medical Condition</td>
+                      <td>{this.state.medical_condition}</td>
+                    </tr>
+                    <tr>
+                      <td>Disease</td>
+                      <td>{this.state.disease}</td>
+                    </tr>
+                    <tr>
+                      <td>Hospital Entry Date</td>
+                      <td>{this.state.hospital_entry}</td>
+                    </tr>
+                    <tr>
+                      <td>Hospital Release Date</td>
+                      <td>{this.state.hospital_release}</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default RecordView;
