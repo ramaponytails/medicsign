f3f8e46dd4e33fb04818fe6231f98e3274bad757
MetalPower26 <MetalPower.github@gmail.com>
2023-02-19 17:51:46 +0700

    Add login

---
+++ frontend/src/app/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const useToken = () => {
+  const getToken = () => {
+    const tokenString = sessionStorage.getItem("token");
+    if (tokenString !== "undefined") {
+      const userToken = JSON.parse(tokenString);
+      return userToken?.token;
+    }
+  };
+
+  const [token, setToken] = useState(getToken());
+
+  const saveToken = (userToken) => {
+    if (userToken !== "undefined") {
+      sessionStorage.setItem("token", JSON.stringify(userToken));
+      setToken(userToken.token);
+    }
+  };
+
+  return {
+    setToken: saveToken,
+    token,
+  };
+};
+
+export default useToken;
