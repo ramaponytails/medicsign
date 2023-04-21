0fcf83c1890debc26f732f7abcdd2a525f5978bf
MetalPower26 <MetalPower.github@gmail.com>
2023-04-21 16:30:51 +0700

    View Text

--- frontend/src/view_record/ViewList.js
+++ frontend/src/view_record/ViewList.js
@@ -74,10 +74,8 @@ class RecordList extends Component {
             <Col md="12">
               <Card className="strpied-tabled-with-hover">
                 <Card.Header>
-                  <Card.Title as="h4">Striped Table with Hover</Card.Title>
-                  <p className="card-category">
-                    Here is a subtitle for this table
-                  </p>
+                  <Card.Title as="h4">Doctor Record List</Card.Title>
+                  <p className="card-category">List of Doctor's Records</p>
                 </Card.Header>
                 <Card.Body className="table-full-width table-responsive px-0">
                   <Table className="table-hover table-striped">
